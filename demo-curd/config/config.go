68a4410dc705b0a59717654d0815fa016792d2a3
chuxin <15115359055@139.com>
2024-04-29 22:24:04 +0800

    feat: 添加注释

--- demo-curd/config/config.go
+++ demo-curd/config/config.go
@@ -12,10 +12,13 @@ var (
 )
 
 func init() {
+	// 定义变量
 	var err error
 
+	// 读取配置
 	Cfg, err = ini.Load("conf/app.ini")
 
+	// 读取的时候如果不是 nil 说明有错误
 	if err != nil {
 		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
 	}
