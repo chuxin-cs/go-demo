1da2a2b88d547ec0bd091fb9435e5d1eb3b315a8
chuxin <15115359055@139.com>
2024-06-20 11:55:35 +0800

    docs: 删除文件重新开始

--- demo-curd/main.go
+++
@@ -1,9 +0,0 @@
-// 指明当前文件属于哪个包
-package main
-
-import "github.com/chuxin-cs/demo-curd/routers"
-
-// 创建 main 方法
-func main() {
-	routers.InitRouter()
-}
--- demo-yaml-v3/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/chuxin-cs/demo-yaml-v3
-
-go 1.20
-
-require gopkg.in/yaml.v3 v3.0.1 // indirect
