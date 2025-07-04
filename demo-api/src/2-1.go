73a8314189f2aac0af48deea7b87947220fa4bba
chuxin <15115359055@139.com>
2025-07-04 17:00:30 +0800

    feat: 添加示例代码并重构主程序入口
    - 新增2-1.go和2-2.go作为基础语法示例文件
    - 修改main.go调用新增的示例函数并移除旧的风险操作代码

---
+++ demo-api/src/2-1.go
@@ -0,0 +1,8 @@
+package core
+
+import "fmt"
+
+func Src21() {
+	// fmt 的方法很多，只列举一个
+	fmt.Println("程序打印")
+}
---
+++ demo-api/src/2-2.go
@@ -0,0 +1,16 @@
+package core
+
+import "fmt"
+
+// 声明 字符串 类型变量
+var age int = 28
+
+// 像ts一样会自动推导，不写类型也没关系
+var isActive = true
+
+func Src22() {
+	name := "云层上的光"
+	fmt.Println("花名==>" + name)
+	fmt.Println(age)
+
+}
